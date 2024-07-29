f7deb32b4c42ead3215be381adc8963828c8568b
Vineet Raj <vineetcres@icloud.com>
2024-07-29 13:19:42 +0530

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".input", {
+    strings:["Internships.","Web Development Projects.", "Video Editing Projects.", "Logo Designing Projects."],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true
+});
